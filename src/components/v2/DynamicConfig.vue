7d830c49593e5d4201924680a99c06f526ac83fb
Mojtaba Sayari <ms.sayyari@gmail.com>
2025-08-11 18:36:00 +0300

    support url input type

--- src/components/v2/DynamicConfig.vue
+++ src/components/v2/DynamicConfig.vue
@@ -6,7 +6,7 @@ import JsonTransformEditor from "@/components/v2/JsonTransformEditor.vue";
 import JsonDerivedColumnsEditor from "@/components/v2/JsonDerivedColumnsEditor.vue";
 import ConditionGroupBuilder from "@/components/v2/ConditionGroupBuilder.vue";
 import ArrayEditor from "@/components/v2/ArrayEditor.vue";
-import { IconEye, IconEyeClosed } from '@tabler/icons-vue'
+import {IconEye, IconEyeClosed} from '@tabler/icons-vue'
 import AISchemaBuilder from "@/components/v2/AISchemaBuilder.vue";
 
 
@@ -27,7 +27,7 @@ const props = defineProps({
 
 const emit = defineEmits(['update:config'])
 
-const localConfig = ref({ ...props.config })
+const localConfig = ref({...props.config})
 const jsonFields = ref({})
 
 const showPassword = ref({})
@@ -39,15 +39,15 @@ const togglePassword = (fieldName) => {
 
 // Watch for external config changes
 watch(() => props.config, (newConfig) => {
-  localConfig.value = { ...newConfig }
+  localConfig.value = {...newConfig}
   // initializeJsonFields()
-}, { deep: true })
+}, {deep: true})
 
 
 // // Emit config on change
 watch(localConfig, (newConfig) => {
   emit('update:config', newConfig)
-}, { deep: true, immediate: true })
+}, {deep: true, immediate: true})
 
 
 // Initialize JSON field strings from config
@@ -60,7 +60,7 @@ const initializeJsonFields = () => {
   }
 }
 
-onUpdated(()=>{
+onUpdated(() => {
   // console.log(props.schema)
 })
 
@@ -75,7 +75,9 @@ onMounted(() => {
   <div class="space-y-4">
     <template v-for="(field, index) in schema" :key="index">
       <div>
-        <label v-if="field.type !== 'boolean'" class="block text-sm font-medium text-gray-700 mb-1">{{ field.label }}</label>
+        <label v-if="field.type !== 'boolean'" class="block text-sm font-medium text-gray-700 mb-1">{{
+            field.label
+          }}</label>
 
         <!-- Selectable String -->
         <select v-if="field.type === 'string' && field.enum"
@@ -85,13 +87,24 @@ onMounted(() => {
         </select>
 
 
+        <!-- URL -->
+        <input v-else-if="field.type === 'url'"
+               v-model="localConfig[field.name]"
+               type="url"
+               :placeholder="field.placeholder || 'https://example.com'"
+               spellcheck="false"
+               pattern="https?://.*"
+               class="w-full px-3 py-2 border border-gray-300 rounded-md"
+        />
+
         <!-- Input field -->
         <input v-else-if="field.type === 'string' || field.type === 'number'"
                v-model="localConfig[field.name]"
-                :type="field.type === 'number' ? 'number' : 'text'"
+               :type="field.type === 'number' ? 'number' : 'text'"
                :placeholder="field.placeholder"
                class="w-full px-3 py-2 border border-gray-300 rounded-md"
         >
+
 
         <!-- Textarea -->
         <textarea v-else-if="field.type === 'textarea' || field.type === 'text'"
@@ -102,24 +115,24 @@ onMounted(() => {
         ></textarea>
 
         <!-- JSON Key-Value -->
-        <array-editor v-else-if="field.type === 'array'" v-model:value="localConfig[field.name]" :schema="field" />
+        <array-editor v-else-if="field.type === 'array'" v-model:value="localConfig[field.name]" :schema="field"/>
 
         <!-- JSON Key-Value -->
         <json-key-value-editor v-else-if="field.type === 'json' && field.format === 'key-value'"
-                               v-model:value="localConfig[field.name]" />
+                               v-model:value="localConfig[field.name]"/>
 
 
         <!-- JSON Condition Builder -->
         <condition-group-builder v-else-if="field.type === 'json' && field.format === 'condition-builder'"
-                                v-model:value="localConfig[field.name]" :schema="field" />
+                                 v-model:value="localConfig[field.name]" :schema="field"/>
 
         <!-- AI Schema Builder -->
         <a-i-schema-builder v-else-if="field.type === 'json' && field.format === 'ai-schema-builder'"
-                               v-model:value="localConfig[field.name]" :schema="field" :is-nested-object="true" />
+                            v-model:value="localConfig[field.name]" :schema="field" :is-nested-object="true"/>
 
         <!-- JSON Filter -->
         <json-filter-editor v-else-if="field.type === 'json' && field.format === 'filter'"
-                            v-model:value="localConfig[field.name]" :schema="field" />
+                            v-model:value="localConfig[field.name]" :schema="field"/>
 
         <!-- JSON Transform -->
         <json-transform-editor v-else-if="field.type === 'json' && field.format === 'transformations'"
@@ -127,13 +140,14 @@ onMounted(() => {
 
         <!-- JSON Derived Columns -->
         <json-derived-columns-editor v-else-if="field.type === 'json' && field.format === 'derived-columns'"
-                                     v-model:value="localConfig[field.name]" />
+                                     v-model:value="localConfig[field.name]"/>
 
         <!-- Select -->
         <select v-else-if="field.type === 'select'"
                 v-model="localConfig[field.name]"
                 class="w-full px-3 py-2 border border-gray-300 rounded-md">
-          <option v-for="opt in field.options" :key="typeof opt === 'object' ? opt.value : opt" :value="typeof opt === 'object' ? opt.value : opt">
+          <option v-for="opt in field.options" :key="typeof opt === 'object' ? opt.value : opt"
+                  :value="typeof opt === 'object' ? opt.value : opt">
             {{ typeof opt === 'object' ? opt.label : opt }}
           </option>
         </select>
@@ -160,11 +174,12 @@ onMounted(() => {
               class="absolute inset-y-0 right-0 flex items-center px-3 text-gray-500"
               tabindex="-1"
           >
-            <icon-eye v-if="showPassword[field.name]" class="h-5 w-5" />
-            <icon-eye-closed v-else class="h-5 w-5" />
+            <icon-eye v-if="showPassword[field.name]" class="h-5 w-5"/>
+            <icon-eye-closed v-else class="h-5 w-5"/>
           </button>
         </div>
-        <div><span v-if="errors['config.'+ field.name]" class="text-red-500 text-sm">{{ errors['config.'+ field.name][0] }}</span></div>
+        <div><span v-if="errors['config.'+ field.name]"
+                   class="text-red-500 text-sm">{{ errors['config.' + field.name][0] }}</span></div>
         <span class="text-sm text-gray-500 ps-1">{{ field.description }}</span>
 
       </div>
